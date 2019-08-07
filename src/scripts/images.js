58c46784fa49700f2d3597038f5ed0161813a02b
marcin.cyboran <marcin.cyboran.wsb@gmail.com>
2019-08-07 20:25:37 +0200

    build

--- src/scripts/images.js
+++
@@ -1,8 +0,0 @@
-// import './../../public/images/favicon.ico';
-// import './../../public/images/dashboard-min.png';
-// import './../../public/images/design-min.png';
-// import './../../public/images/code-min.png';
-// import './../../public/images/enjoy-min.png';
-// import './../../public/images/contact-min.png';
-// import './../../public/images/tnt@2x-min.png';
-// import './../../public/images/tnt-min.png';
